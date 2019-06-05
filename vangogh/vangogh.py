c11fe43a70be4f612d62489e32274f96e36b1907
proprefenetre <proprefenetre@gmail.com>
2019-06-05 14:22:30 +0200

    Letter class

--- vangogh/vangogh.py
+++ vangogh/vangogh.py
@@ -1,17 +1,17 @@
-from collections import defaultdict
 from lxml import etree
 import re
 
-parser = etree.XMLParser(attribute_defaults=True)
-tree = etree.parse("/home/niels/projects/vangogh/letters/let001a.xml", parser)
+# tree = etree.parse("/home/niels/projects/vangogh/letters/let001a.xml", parser)
 
-NS = {"t": "{http://www.tei-c.org/ns/1.0}"}
+class Letter:
 
-doc = tree.getroot()
+    def __init__(self, xml, parser=etree.XMLParser(attribute_defaults=True), namespace={"t": "{http://www.tei-c.org/ns/1.0}"}):
+        self.parser = parser
+        self.xml = etree.parse(xml, parser)
+        self.namespace = namespace
 
-names = defaultdict(set)
-for e in doc.iter(tag=f"{NS['t']}rs"):
-   names[e.get('key')].add(re.sub(r"\s+", r" ", e.text))
-
-# for e in doc.iter(tag=f"{NS['t']}div"):
-#     print(e.text)
+    def people(self):
+        names = {}
+        for e in doc.iter(tag=f"{self.namespace['t']}rs"):
+            names[re.sub(r'\s+', r' ', e.text)] = e.get('key')
+        return names
