dfebe0cdb4f793f493f2250b326f05133aba27c2
proprefenetre <proprefenetre@gmail.com>
2019-06-05 13:01:58 +0200

    extract names with their reference keys

--- vangogh/vangogh.py
+++ vangogh/vangogh.py
@@ -1,10 +1,17 @@
+from collections import defaultdict
 from lxml import etree
 import re
 
 parser = etree.XMLParser(attribute_defaults=True)
 tree = etree.parse("/home/niels/projects/vangogh/letters/let001a.xml", parser)
 
-TEI_NS = "{http://www.tei-c.org/ns/1.0}"
+NS = {"t": "{http://www.tei-c.org/ns/1.0}"}
+
 doc = tree.getroot()
-for e in doc.iter(tag=f"{TEI_NS}rs"):
-   print(re.sub(r"\s+", r" ", e.text))
+
+names = defaultdict(set)
+for e in doc.iter(tag=f"{NS['t']}rs"):
+   names[e.get('key')].add(re.sub(r"\s+", r" ", e.text))
+
+# for e in doc.iter(tag=f"{NS['t']}div"):
+#     print(e.text)
