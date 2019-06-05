9a4297b12e23c25a28d41d4732ccc828e4c9b4da
proprefenetre <proprefenetre@gmail.com>
2019-06-05 16:44:11 +0200

    strip leestekens & verwijder textualNotes

--- vangogh/vangogh.py
+++ vangogh/vangogh.py
@@ -1,8 +1,6 @@
 from lxml import etree
 import re
 
-# tree = etree.parse("/home/niels/projects/vangogh/letters/let001a.xml", parser)
-
 class Letter:
 
     def __init__(self, xml, parser=etree.XMLParser(attribute_defaults=True), namespaces={"tei": "{http://www.tei-c.org/ns/1.0}"}):
@@ -17,13 +15,16 @@ class Letter:
         return names
 
     def original_text(self):
+        find_textual_notes = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"textualNotes\"]")
+        for e in find_textual_notes(self.xml):
+            e.getparent().remove(e)
         find = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"original\"]//text()")
         text = ''.join(find(self.xml))
-        text = re.sub(r'\s+', r' ', text)
-        text = re.sub(r'[–&.,!?;()]+', r'', text)
+        text = re.sub(r'[\s’]+', r' ', text)
+        text = re.sub(r'[–&.,!?;()\\\/─_“”‘-]+', r'', text)
         return text.lower()
 
 
-
-l = Letter("/home/niels/projects/vangogh/letters/let001a.xml")
-print(l.original_text())
+for n in range(1, 11):
+    l = Letter(f"/home/niels/projects/vangogh/letters/let0{n:0>2}.xml")
+    print(l.original_text())
