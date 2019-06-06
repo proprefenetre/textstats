f89d5c243d0f825c3924d97900c1b578b07fd231
proprefenetre <proprefenetre@gmail.com>
2019-06-06 11:54:52 +0200

    Move Letter to its own file

---
+++ vangogh/letter.py
@@ -0,0 +1,25 @@
+from lxml import etree
+import re
+
+class Letter:
+
+    def __init__(self, xml, parser=etree.XMLParser(attribute_defaults=True), namespaces={"tei": "{http://www.tei-c.org/ns/1.0}"}):
+        self.xml = etree.parse(xml, parser)
+        self.namespaces = namespaces
+
+    def people(self):
+        names = {}
+        find_names = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}rs")
+        for e in find_names(self.xml):
+            names[re.sub(r'\s+', r' ', e.text)] = e.get('key')
+        return names
+
+    def original_text(self):
+        find_textual_notes = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"textualNotes\"]")
+        for e in find_textual_notes(self.xml):
+            e.getparent().remove(e)
+        find = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"original\"]//text()")
+        text = ''.join(find(self.xml))
+        # text = re.sub(r'[\s’]+', r' ', text)
+        # text = re.sub(r'[–&.,!?;()\\\/─_“”‘-]+', r'', text)
+        return text
--- vangogh/vangogh.py
+++ vangogh/vangogh.py
@@ -1,30 +1,8 @@
-from lxml import etree
-import re
+import spacy
 
-class Letter:
+corpus = [Letter(f"/home/niels/projects/vangogh/letters/let0{n:0>2}.xml").original_text() for  n in range(1, 11)]
 
-    def __init__(self, xml, parser=etree.XMLParser(attribute_defaults=True), namespaces={"tei": "{http://www.tei-c.org/ns/1.0}"}):
-        self.xml = etree.parse(xml, parser)
-        self.namespaces = namespaces
-
-    def people(self):
-        names = {}
-        find = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}rs")
-        for e in find(self.xml):
-            names[re.sub(r'\s+', r' ', e.text)] = e.get('key')
-        return names
-
-    def original_text(self):
-        find_textual_notes = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"textualNotes\"]")
-        for e in find_textual_notes(self.xml):
-            e.getparent().remove(e)
-        find = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"original\"]//text()")
-        text = ''.join(find(self.xml))
-        text = re.sub(r'[\s’]+', r' ', text)
-        text = re.sub(r'[–&.,!?;()\\\/─_“”‘-]+', r'', text)
-        return text.lower()
-
-
-for n in range(1, 11):
-    l = Letter(f"/home/niels/projects/vangogh/letters/let0{n:0>2}.xml")
-    print(l.original_text())
+nlp = spacy.load('nl_core_news_sm')
+l = nlp(corpus[0])
+for t in l:
+    print(t.text, t.pos_, t.dep_)
