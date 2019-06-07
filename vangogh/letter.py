140b0755b8d33c0c31cdac245b7ecb4ea3d54d0f
proprefenetre <proprefenetre@gmail.com>
2019-06-07 17:44:11 +0200

    rare interpunctie zoeken

--- vangogh/letter.py
+++ vangogh/letter.py
@@ -15,16 +15,20 @@ class Letter:
             names[re.sub(r'\s+', r' ', e.text)] = e.get('key')
         return names
 
-    def original_text(self, strip=False, lower=False):
+    def original_text(self):
         find_textual_notes = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"textualNotes\"]")
         for e in find_textual_notes(self.xml):
             e.getparent().remove(e)
         find = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"original\"]//text()")
         text = ''.join(find(self.xml))
         if strip:
-            # strip whitespace & punctuation
             text = re.sub(r'[\s’]+', r' ', text)
             text = re.sub(rf'[\–\─\-“”‘{string.punctuation}]+', r'', text)
         if lower:
             text = text.lower()
         return text
+
+    def rare_interpunctie(self):
+        find_c = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}c")
+        for e in find_c(self.xml):
+            return (e.get('type'), e.text)
--- vangogh/vangogh.py
+++ vangogh/vangogh.py
@@ -1,5 +1,7 @@
 from pprint import pprint
+
 import spacy
+from spacy.lang.nl.stop_words import STOP_WORDS
 
 from letter import Letter
 
@@ -7,12 +9,4 @@ corpus = [Letter(f"/home/niels/projects/vangogh/letters/let0{n:0>2}.xml") for n 
 
 nlp = spacy.load('nl_core_news_sm')
 
-l = nlp(corpus[0].original_text())
-l_strip = nlp(corpus[0].original_text(strip=True))
-l_lower = nlp(corpus[0].original_text(strip=True, lower=True))
-
-ents = [(e.text, e.start_char, e.end_char, e.label_) for e in l.ents]
-s_ents = [(e.text, e.start_char, e.end_char, e.label_) for e in l_strip.ents]
-
-pprint(ents)
-pprint(s_ents)
+text = nlp(''.join([l.original_text(process=True) for l in corpus]))
