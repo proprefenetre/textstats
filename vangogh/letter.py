98a80c0dcf8b78ee6b88a632e416c30ef6de1a89
proprefenetre <proprefenetre@gmail.com>
2019-06-11 13:45:20 +0200

    add preprocessing

--- vangogh/letter.py
+++ vangogh/letter.py
@@ -1,14 +1,18 @@
 from lxml import etree
 import re
 import string
+import sys
+import unicodedata
 
 class Letter:
 
-    def __init__(self, xml, parser=etree.XMLParser(attribute_defaults=True), namespaces={"tei": "{http://www.tei-c.org/ns/1.0}"}):
+    def __init__(self, xml, parser=etree.XMLParser(attribute_defaults=True)):
         self.xml = etree.parse(xml, parser)
-        self.namespaces = namespaces
 
     def people(self):
+        """
+        Extract names of people mentioned in the letters
+        """
         names = {}
         find_names = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}rs")
         for e in find_names(self.xml):
@@ -16,19 +20,73 @@ class Letter:
         return names
 
     def original_text(self):
+        """
+        Return transcription (without textual notes)
+        """
         find_textual_notes = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"textualNotes\"]")
         for e in find_textual_notes(self.xml):
             e.getparent().remove(e)
+
         find = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}div[@type=\"original\"]//text()")
         text = ''.join(find(self.xml))
-        if strip:
-            text = re.sub(r'[\s’]+', r' ', text)
-            text = re.sub(rf'[\–\─\-“”‘{string.punctuation}]+', r'', text)
-        if lower:
-            text = text.lower()
         return text
 
-    def rare_interpunctie(self):
-        find_c = etree.ETXPath("//{http://www.tei-c.org/ns/1.0}c")
-        for e in find_c(self.xml):
-            return (e.get('type'), e.text)
+    def _whitespace(self, text):
+        return re.sub("\s+", " ", text)
+
+    def _punctuation(self, text):
+        """
+        remove unicode & ascii punctuation, except for apostrophes
+        """
+        unicode_punct = [
+            '\u00a0', # NO-BREAK SPACE
+            '\u00a3', # POUND SIGN
+            '\u00b0', # DEGREE SIGN
+            '\u00b1', # PLUS-MINUS SIGN
+            '\u00b4', # ACUTE ACCENT
+            '\u00b7', # MIDDLE DOT
+            '\u00bb', # RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK
+            '\u00bd', # VULGAR FRACTION ONE HALF
+            '\u2013', # EN DASH
+            '\u2014', # EM DASH
+            '\u2018', # LEFT SINGLE QUOTATION MARK
+            '\u201c', # LEFT DOUBLE QUOTATION MARK
+            '\u201d', # RIGHT DOUBLE QUOTATION MARK
+            '\u2026', # HORIZONTAL ELLIPSIS
+            '\u2500', # BOX DRAWINGS LIGHT HORIZONTAL
+            '\u25a1', # WHITE SQUARE
+            # '\u2019', # RIGHT SINGLE QUOTATION MARK
+        ]
+        string_punct = list(string.punctuation)
+        return re.sub(rf"[{''.join(unicode_punct + string_punct)}]", "", text)
+
+    def _apostrophes(self, text):
+        """
+        Replace RIGHT SINGLE QUOTATION MARK with ascii apostrophe
+        """
+        return re.sub('\u2019', '\'', text)
+
+    def _diacritics(self, text):
+        """
+        Replace characters with diacritical marks with their ascii equivalents
+        """
+        return "".join(c for c in unicodedata.normalize("NFKD", text) if not unicodedata.combining(c))
+
+    def preprocess(self, levels=['whitespace', 'punctuation', 'apostrophes', 'diacritics']):
+        """
+        preprocess the text,
+
+        """
+        dispatch = {'whitespace': self._whitespace,
+                    'punctuation': self._punctuation,
+                    'apostrophes': self._apostrophes,
+                    'diacritics': self._diacritics}
+
+        text = self.original_text()
+        for l in levels:
+            text = dispatch[l](text)
+        # text = self._whitespace(text)
+        # text = self._punctuation(text)
+        # text = self._apostrophes(text)
+        # text = self._diacritics(text)
+        return text
--- vangogh/vangogh.py
+++ vangogh/vangogh.py
@@ -1,12 +1,14 @@
-from pprint import pprint
+from codepoints import process
 
+from pprint import pprint
+import re
 import spacy
 from spacy.lang.nl.stop_words import STOP_WORDS
 
+# nlp = spacy.load('nl_core_news_sm')
+
 from letter import Letter
 
-corpus = [Letter(f"/home/niels/projects/vangogh/letters/let0{n:0>2}.xml") for n in range(1, 11)]
+corpus = [Letter(f"/home/niels/projects/vangogh/letters/let{n:0>3}.xml") for n in range(1, 11)]
 
-nlp = spacy.load('nl_core_news_sm')
-
-text = nlp(''.join([l.original_text(process=True) for l in corpus]))
+text = [l.preprocess() for l in corpus]
